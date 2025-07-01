09fb22246d4372c4639431ffce3b57bc795ea6f8
Dimas Tri Mustakim <test31122001@gmail.com>
2025-07-01 07:30:53 +0700

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    vite: {
+        // @ts-ignore
+        plugins: [tailwindcss()],
+    },
+    site: "https://example.com",
+    integrations: [mdx(), sitemap()],
 });
