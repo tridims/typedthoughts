a791d73046d1a87dc1c324d5487463566f95b8ba
Dimas Tri Mustakim <test31122001@gmail.com>
2026-06-30 21:16:46 +0700

    lint & fix files using biome & ultracite

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,25 +1,28 @@
-import { glob } from 'astro/loaders';
-import { defineCollection } from 'astro:content';
-import { z } from 'astro/zod';
+import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) => z.object({
-		title: z.string(),
-		description: z.string(),
-		shorts: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.object({
-			image: image(),
-			source: z.string().optional(),
-			owner: z.string().optional(),
-		}).optional(),
-		externalPost: z.string().optional(),
-	}),
+  // Load Markdown and MDX files in the `src/content/blog/` directory.
+  loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
+  // Type-check frontmatter using a schema
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      shorts: z.string(),
+      // Transform string to Date object
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      heroImage: z
+        .object({
+          image: image(),
+          source: z.string().optional(),
+          owner: z.string().optional(),
+        })
+        .optional(),
+      externalPost: z.string().optional(),
+    }),
 });
 
 export const collections = { blog };
